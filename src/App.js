899ba4b208a29fb2586c7f2eba5fd0844aac3d23
aschulz2132 <aschulz2132@gmail.com>
2023-01-12 21:19:58 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>rizzlers.org</h1>
+      <br />
+      <h5>play:</h5>
+      <br />
+      <br />
+      <p>mc.rizzlers.org</p>
     </div>
   );
 }
